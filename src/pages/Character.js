f976a73208246e7a3fa35e854825a0bcacc12590
Francisco López <flopez_1384@hotmail.com>
2020-07-30 21:49:54 -0500

    [CHANGE] Styles for the character cards

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -15,8 +15,8 @@ const Character = async () => {
         <h3>Status: <span> ${character.status}</span></h3>
         <h3>Species: <span> ${character.species}</span></h3>
         <h3>Gender: <span> ${character.gender}</span></h3>
-        <h3>Last Location: <span> ${character.location.name}</span></h3>
         <h3>Origin: <span> ${character.origin.name}</span></h3>
+        <h3>Last Location: <span> ${character.location.name}</span></h3>
       </article>
     </div>
   `;
